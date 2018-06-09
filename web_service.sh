d994bbc6d1d2ec901333c13910bb6a17793bd84f
titipata <my.titipat@gmail.com>
2018-06-09 19:18:26 -0400

    Add static html back

--- web_service.sh
+++ web_service.sh
@@ -1,6 +1,7 @@
 python -m allennlp.service.server_simple \
     --archive-path model.tar.gz \
     --predictor discourse_classifier \
+    --static-dir static_html \
     --include-package discourse \
     --title "Discourse Prediction" \
     --field-name sentence
