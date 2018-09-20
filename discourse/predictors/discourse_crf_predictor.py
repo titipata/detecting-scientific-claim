88af7be3c9915b5aef02633c85e2d5eea9f91d2b
Titipat Achakulvisut <my.titipat@gmail.com>
2018-09-20 12:25:08 -0400

    import spacy for sentence tokenization

--- discourse/predictors/discourse_crf_predictor.py
+++ discourse/predictors/discourse_crf_predictor.py
@@ -1,10 +1,12 @@
+import spacy
 from typing import Tuple
-
 from overrides import overrides
 
 from allennlp.common.util import JsonDict
 from allennlp.data import Instance
 from allennlp.predictors.predictor import Predictor
+
+nlp = spacy.load('en_core_web_sm')
 
 @Predictor.register('discourse_crf_classifier')
 class DiscourseCRFClassifierPredictor(Predictor):
@@ -14,6 +16,7 @@ class DiscourseCRFClassifierPredictor(Predictor):
     @overrides
     def _json_to_instance(self, json_dict: JsonDict) -> Instance:
         abstract = json_dict["abstract"]
+        abstract = nlp(abstract)
         sentences = [sent.text.strip() for sent in abstract.sents]
         instance = self._dataset_reader.text_to_instance(sentences=sentences)
         return instance
