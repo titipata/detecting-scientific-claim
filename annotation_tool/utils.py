fc1e00a3256627b5f75011a043055e1c56233661
titipata <my.titipat@gmail.com>
2018-11-09 15:31:12 -0500

    Inter-annotation agreement dataset

--- annotation_tool/utils.py
+++ annotation_tool/utils.py
@@ -1,9 +1,12 @@
+from typing import List
 import os
 import sys
 import json
+import pandas as pd
 from itertools import chain
 from lxml import etree, html
 from urllib.request import urlopen
+from sklearn.metrics import cohen_kappa_score
 
 
 PUBMED_URL = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&retmode=xml&id=%s'
@@ -55,3 +58,24 @@ def save_json(ls, file_path):
     """
     with open(file_path, 'w') as fp:
         fp.write('\n'.join(json.dumps(i) for i in ls))
+
+
+def calculate_cohen_kappa_score(df: pd.DataFrame, users: List = []):
+    """
+    Calculage inter-annotation agreement given the labels dataframe
+
+    dataframe columns include "user_id", "paper_id", "labels" (List)
+
+    >> calculate_cohen_kappa_score(df, users=['user1', 'user2'])
+    """
+    assert(len(users) == 2)
+    annotation_sel_df = df[df['user_id'].map(lambda x: x in users)]
+    size_df = annotation_sel_df.groupby('paper_id').size().reset_index()
+    annotation_sel_df = annotation_sel_df.merge(size_df[size_df[0] == 2][['paper_id']])
+
+    annotations_1, annotations_2 = [], []
+    for _, df in annotation_sel_df.groupby('paper_id'):
+        annotations_1.extend(df.labels.iloc[0])
+        annotations_2.extend(df.labels.iloc[1])
+
+    return cohen_kappa_score(annotations_1, annotations_2)
