af473c84c16e93c92b7253ac228cce15feb71621
titipata <my.titipat@gmail.com>
2018-11-27 11:31:18 -0500

    Add utils function in discourse library

--- discourse/__init__.py
+++ discourse/__init__.py
@@ -1,3 +1,4 @@
 from discourse.dataset_readers import *
 from discourse.models import *
 from discourse.predictors import *
+from .utils import read_json
---
+++ discourse/utils.py
@@ -0,0 +1,13 @@
+import os
+import json
+
+def read_json(file_path):
+    """
+    Read list from JSON path
+    """
+    if not os.path.exists(file_path):
+        return []
+    else:
+        with open(file_path, 'r') as fp:
+            ls = [json.loads(line) for line in fp]
+        return ls
