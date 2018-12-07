3d9badab405db6b6225d23d8dda5955bcb835df7
titipata <my.titipat@gmail.com>
2018-12-07 13:05:19 -0500

    Update flask application, experiment files

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ from discourse.predictors import DiscourseClassifierPredictor
 
 
 TESTING = False # if true, run testing
-EMBEDDING_DIM = 200
+EMBEDDING_DIM = 300
 PUBMED_URL = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&retmode=xml&id=%s'
 DISCOURSE_MODEL_PATH = 'https://s3-us-west-2.amazonaws.com/pubmed-rct/model_crf.tar.gz'
 WEIGHT_PATH = 'https://s3-us-west-2.amazonaws.com/pubmed-rct/model_crf_tf.th'
@@ -60,12 +60,10 @@ if not TESTING:
     for param in list(model.parameters()):
         param.requires_grad = False
     num_classes, constraints, include_start_end_transitions = 2, None, False
-    model.classifier_feedforward._linear_layers = ModuleList([torch.nn.Linear(2 * EMBEDDING_DIM, EMBEDDING_DIM), 
-                                                              torch.nn.Linear(EMBEDDING_DIM, num_classes)])
     model.crf = ConditionalRandomField(num_classes, constraints, 
                                     include_start_end_transitions=include_start_end_transitions)
     model.label_projection_layer = TimeDistributed(Linear(2 * EMBEDDING_DIM, num_classes))
-    model.load_state_dict(torch.load(cached_path(WEIGHT_PATH)))
+    model.load_state_dict(torch.load(cached_path(WEIGHT_PATH), map_location='cpu'))
     reader = CrfPubmedRCTReader()
     claim_predictor = ClaimCrfPredictor(model, dataset_reader=reader)
 
